4431767bd9ed592f9c84f7bf731bb0904eea8c6f
Russel Jerome Rapi <111546620+syllablelous@users.noreply.github.com>
2022-09-06 09:20:37 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    String surname;
+    String firstName;
+    char middleInitial;
+    String dateOfBirth;
+    int studentNumber;
+    String studentEmailAddress;
+    boolean iAmAwesome;
+
+    public void sayMyStudentNumber(){
+        System.out.println("My student number is: " + studentNumber);
+    }
+    public void sayMyEmailAddress(){
+        System.out.println("My email address is: " + studentEmailAddress);
+    }
+    public void amIAwesome(){
+        System.out.println("Am I really awesome? " + iAmAwesome);
+    }
+}
